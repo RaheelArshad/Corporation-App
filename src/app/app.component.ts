49c3952023ebe2f918bce955e318f599df47fb7b
Raheel arshad <raheelarshad8@gmail.com>
2020-04-26 17:09:04 +0400

    puah notification with Ionic 5 and angular 9 & PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import {environment} from '../environments/environment';
+import {firebase} from '@firebase/app';
+import { NotificationsService } from './notifications.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,10 +15,24 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private notificationsService: NotificationsService
   ) {
+
     this.initializeApp();
   }
+
+  async ngOnInit() {
+    firebase.initializeApp(environment.firebase);
+    await this.notificationsService.init();
+}
+
+
+ngAfterViewInit() {
+  this.platform.ready().then(async () => {
+     await this.notificationsService.requestPermission();
+  });
+}
 
   initializeApp() {
     this.platform.ready().then(() => {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDvLTZMJZJ6ddH5IiMPKnUIJojkC_djg_g',
+     authDomain: 'pushnotification-d3034.firebaseapp.com',
+     databaseURL: 'https://pushnotification-d3034.firebaseio.com',
+      projectId: 'pushnotification-d3034',
+      storageBucket: 'pushnotification-d3034.appspot.com',
+      messagingSenderId: '99658086870',
+      appId: '1:99658086870:web:07b479b335eb66ca108c77',
+      measurementId: 'G-P8M7M9QG7N',
+      vapidKey: 'BGRPhkSil4RCV_TKr8a7r5l069UO0FZX_M1LZfIZ_xGSgVAbpv4Z3AMjkkzQHw3iSRGgyCoYKtH-j0ATSDcqo7s'
+  }
 };
